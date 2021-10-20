7a277343cbcec2b12a1d6e76304a03fd6e9c729f
Jonas Jongejan <jongejan@google.com>
2021-10-20 10:45:30 -0400

    chore: Change to spaces

--- files/entry.js
+++ files/entry.js
@@ -4,59 +4,59 @@ import {getRawBody} from '@sveltejs/kit/node';
 import {init, render} from '../output/server/app.js';
 
 function createKitMiddleware({render}) {
-	return async (request, response) => {
-		let parsed;
-		try {
-			parsed = new URL(request.url || '', 'http://localhost');
-		} catch {
-			response.statusCode = 400;
-			return response.end('Invalid URL');
-		}
+  return async (request, response) => {
+    let parsed;
+    try {
+      parsed = new URL(request.url || '', 'http://localhost');
+    } catch {
+      response.statusCode = 400;
+      return response.end('Invalid URL');
+    }
 
-		let body;
+    let body;
 
-		try {
-			body = await getRawBody(request);
-		} catch (error) {
-			response.statusCode = error.status || 400;
-			return response.end(error.reason || 'Invalid request body');
-		}
+    try {
+      body = await getRawBody(request);
+    } catch (error) {
+      response.statusCode = error.status || 400;
+      return response.end(error.reason || 'Invalid request body');
+    }
 
-		const rendered = await render({
-			method: request.method,
-			headers: request.headers,
-			path: parsed.pathname,
-			query: parsed.searchParams,
-			rawBody: body,
-		});
+    const rendered = await render({
+      method: request.method,
+      headers: request.headers,
+      path: parsed.pathname,
+      query: parsed.searchParams,
+      rawBody: body,
+    });
 
-		if (rendered) {
-			response.writeHead(rendered.status, rendered.headers);
-			if (rendered.body) {
-				response.write(rendered.body);
-			}
+    if (rendered) {
+      response.writeHead(rendered.status, rendered.headers);
+      if (rendered.body) {
+        response.write(rendered.body);
+      }
 
-			response.end();
-		} else {
-			response.statusCode = 404;
-			response.end('Not found');
-		}
-	};
+      response.end();
+    } else {
+      response.statusCode = 404;
+      response.end('Not found');
+    }
+  };
 }
 
 init();
 const kitMiddleware = createKitMiddleware({render});
 
 const server = polka().use(
-	compression({threshold: 0}),
-	kitMiddleware,
+  compression({threshold: 0}),
+  kitMiddleware,
 );
 
 const port = process.env.PORT || 8080;
 const listenOptions = {port};
 
 server.listen(listenOptions, () => {
-	console.log(`Listening on ${port}`);
+  console.log(`Listening on ${port}`);
 });
 
 export {server};
--- index.js
+++ index.js
@@ -11,92 +11,92 @@ import glob from 'glob-promise';
 
 /** @type {import('.')} **/
 export default function entrypoint(options) {
-	return {
-		name: 'appengine',
+  return {
+    name: 'appengine',
 
-		async adapt({utils}) {
-			const dir = '.appengine_build_output';
-			utils.rimraf(dir);
+    async adapt({utils}) {
+      const dir = '.appengine_build_output';
+      utils.rimraf(dir);
 
-			const files = fileURLToPath(new URL('./files', import.meta.url));
+      const files = fileURLToPath(new URL('./files', import.meta.url));
 
-			const dirs = {
-				static: join(dir, 'static'),
-				client: join(dir, 'client'),
-				tmp: '.svelte-kit/appengine/',
-			};
+      const dirs = {
+        static: join(dir, 'static'),
+        client: join(dir, 'client'),
+        tmp: '.svelte-kit/appengine/',
+      };
 
-			utils.log.minor('Generating nodejs entrypoint...');
-			utils.rimraf(dirs.tmp);
-			utils.copy(join(files, 'entry.js'), '.svelte-kit/appengine/entry.js');
+      utils.log.minor('Generating nodejs entrypoint...');
+      utils.rimraf(dirs.tmp);
+      utils.copy(join(files, 'entry.js'), '.svelte-kit/appengine/entry.js');
 
-			/** @type {BuildOptions} */
-			const defaultOptions = {
-				entryPoints: [join(dirs.tmp, 'entry.js')],
-				outfile: join(dir, 'index.js'),
-				bundle: true,
-				inject: [join(files, 'shims.js')],
-				platform: 'node',
-				target: 'node16',
-			};
+      /** @type {BuildOptions} */
+      const defaultOptions = {
+        entryPoints: [join(dirs.tmp, 'entry.js')],
+        outfile: join(dir, 'index.js'),
+        bundle: true,
+        inject: [join(files, 'shims.js')],
+        platform: 'node',
+        target: 'node16',
+      };
 
-			const buildOptions = options && options.esbuild
-				? await options.esbuild(defaultOptions) : defaultOptions;
+      const buildOptions = options && options.esbuild
+        ? await options.esbuild(defaultOptions) : defaultOptions;
 
-			await esbuild.build(buildOptions);
+      await esbuild.build(buildOptions);
 
-			utils.log.minor('Writing package.json...');
-			writeFileSync(
-				join(dir, 'package.json'),
-				JSON.stringify({
-					type: 'commonjs',
-				}),
-			);
+      utils.log.minor('Writing package.json...');
+      writeFileSync(
+        join(dir, 'package.json'),
+        JSON.stringify({
+          type: 'commonjs',
+        }),
+      );
 
-			utils.log.minor('Prerendering static pages...');
-			await utils.prerender({
-				dest: dirs.static,
-			});
+      utils.log.minor('Prerendering static pages...');
+      await utils.prerender({
+        dest: dirs.static,
+      });
 
-			utils.log.minor('Copying assets...');
-			utils.copy_static_files(dirs.static);
-			utils.copy_client_files(dirs.client);
+      utils.log.minor('Copying assets...');
+      utils.copy_static_files(dirs.static);
+      utils.copy_client_files(dirs.client);
 
-			utils.log.minor('Writing app.yaml...');
+      utils.log.minor('Writing app.yaml...');
 
-			const staticFiles = await glob('**/*.*', {cwd: dirs.static});
-			utils.log.minor('Creating routes for static files' + staticFiles.join(', '));
+      const staticFiles = await glob('**/*.*', {cwd: dirs.static});
+      utils.log.minor('Creating routes for static files' + staticFiles.join(', '));
 
-			const staticFilesRoutes = staticFiles.map(f => ({
-				// Remove index.html from url
-				url: '/' + f.replace(/index\.html$/gi, ''),
-				// eslint-disable-next-line camelcase
-				static_files: join('static', f),
-				upload: join('static', f),
-			}));
+      const staticFilesRoutes = staticFiles.map(f => ({
+        // Remove index.html from url
+        url: '/' + f.replace(/index\.html$/gi, ''),
+        // eslint-disable-next-line camelcase
+        static_files: join('static', f),
+        upload: join('static', f),
+      }));
 
-			writeFileSync(
-				join(dir, 'app.yaml'),
-				YAML.stringify({
-					runtime: 'nodejs16',
-					entrypoint: 'node index.js',
-					handlers: [
-						{
-							url: '/_app',
-							// eslint-disable-next-line camelcase
-							static_dir: 'client/_app',
-						},
-						...staticFilesRoutes,
-						{
-							url: '/.*',
-							secure: 'always',
-							script: 'auto',
-						},
-					],
-				}),
-			);
+      writeFileSync(
+        join(dir, 'app.yaml'),
+        YAML.stringify({
+          runtime: 'nodejs16',
+          entrypoint: 'node index.js',
+          handlers: [
+            {
+              url: '/_app',
+              // eslint-disable-next-line camelcase
+              static_dir: 'client/_app',
+            },
+            ...staticFilesRoutes,
+            {
+              url: '/.*',
+              secure: 'always',
+              script: 'auto',
+            },
+          ],
+        }),
+      );
 
-			utils.log.success('To deploy, run "gcloud app deploy --project <CLOUD_PROJECT_ID> .appengine_build_output/app.yaml"');
-		},
-	};
+      utils.log.success('To deploy, run "gcloud app deploy --project <CLOUD_PROJECT_ID> .appengine_build_output/app.yaml"');
+    },
+  };
 }
