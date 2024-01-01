cd3a9699e7364de9aa55dd89b8f908b2c46f6437
Jonas Jongejan <jonas@picturestudio.ai>
2024-01-01 16:13:22 -0500

    chore: Migrate from xo to eslint

--- tests/create-svelte.js
+++ tests/create-svelte.js
@@ -1,15 +1,18 @@
-import process from 'node:process';
-import {create} from 'create-svelte';
+import process from "node:process";
+// eslint-disable-next-line import/no-unresolved, node/no-missing-import
+import { create } from "create-svelte";
 
 // Get target directory
 const target = process.argv[2];
 
-await create(target, {
-  name: 'my-new-app',
-  template: 'default', // Or 'skeleton' or 'skeletonlib'
-  types: 'checkjs', // Or 'typescript' or null;
-  prettier: false,
-  eslint: false,
-  playwright: false,
-  vitest: false,
-});
+(async () => {
+  await create(target, {
+    name: "my-new-app",
+    template: "default", // Or 'skeleton' or 'skeletonlib'
+    types: "checkjs", // Or 'typescript' or null;
+    prettier: false,
+    eslint: false,
+    playwright: false,
+    vitest: false,
+  });
+})();
